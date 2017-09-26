0be46f7f45dbe38b28fb4bc6a496cc75e22e2267
djseo1995 <djseo1995@naver.com>
2017-09-26 22:31:49 +0900

    [Modified] argument as device

--- main.c
+++ main.c
@@ -5,10 +5,6 @@ int main(int argc, char *argv[])
 	pcap_t *handle;			/* Session handle */
 	char *dev;			/* The device to sniff on */
 	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-	struct bpf_program fp;		/* The compiled filter */
-	char filter_exp[] = "port 80";	/* The filter expression */
-	bpf_u_int32 mask;		/* Our netmask */
-	bpf_u_int32 net;		/* Our IP */
 	struct pcap_pkthdr *header;	/* The header that pcap gives us */
 	const u_char *packet;		/* The actual packet */
 	struct ether_header *eptr;
@@ -16,28 +12,20 @@ int main(int argc, char *argv[])
 	struct tcphdr *tptr;
 	const u_char *dptr;
 
-		/* Define the device */
-	dev = pcap_lookupdev(errbuf);
-	if (dev == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		return(2);
+	if(argc != 2 ) {
+		printf("Usage : ./pcap_test <device>\n");
+		return -1;
 	}
-		/* Find the properties for the device */
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-		net = 0;
-		mask = 0;
+	
+	handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
+	if(handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s : %s \n", dev, errbuf);
+		return -1;
 	}
-		/* Open the session in promiscuous mode */
-	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-		return(2);
-	}
+
 	while (1) {	/* Grab a packet */
 		pcap_next_ex(handle, &header, &packet);
 		eptr = (struct eptr_header *) packet;
-		/* Print its length */
 		printf("-----------------------------------\n");
 		printf("Jacked a packet with length of [%d]\n", header->len);
 		printf("ether type : 0x%04x\n", ntohs(eptr->ether_type));
