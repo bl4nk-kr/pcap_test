561af43cd839b81f34eb7a391075b31ff528b908
bl4nk-kr <bl4nk.kr@gmail.com>
2017-09-26 11:48:53 -0700

    [Modified] enhanced readability

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include "pcap_test.h"
 
-int main(int argc, char *argv[])
-{
-	pcap_t *handle;			/* Session handle */
-	char *dev;			/* The device to sniff on */
-	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-	struct pcap_pkthdr *header;	/* The header that pcap gives us */
-	const u_char *packet;		/* The actual packet */
+int main(int argc, char *argv[]) {
+	pcap_t *handle;
+	char *dev;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	struct pcap_pkthdr *header;
+	const u_char *packet;
 	struct ether_header *eptr;
 	struct ip *iptr;
 	struct tcphdr *tptr;
@@ -23,9 +22,11 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	while (1) {	/* Grab a packet */
+	while (1) {
 		pcap_next_ex(handle, &header, &packet);
+
 		eptr = (struct eptr_header *) packet;
+		
 		printf("-----------------------------------\n");
 		printf("Jacked a packet with length of [%d]\n", header->len);
 		printf("ether type : 0x%04x\n", ntohs(eptr->ether_type));
@@ -33,14 +34,18 @@ int main(int argc, char *argv[])
 		p_colon(eptr->ether_shost);
 		printf("eth.dmac : ");
 		p_colon(eptr->ether_dhost);
+		
 		if(eptr->ether_type == IPTYPE) {
 			iptr = (struct ip *) (packet + 14);
+			
 			printf("ip protocol : %d\n", iptr->ip_p);
 			printf("ip.sip : %s\n", inet_ntoa(iptr->ip_src));
 			printf("ip.dip : %s\n", inet_ntoa(iptr->ip_dst));
+			
 			if(iptr->ip_p == TCPTYPE) {
 				tptr = (struct tcphdr *) (packet + 14 + (iptr->ip_hl * 4));
 				dptr = packet + 14 + (iptr->ip_hl * 4) + (tptr->th_off * 4);
+				
 				printf("tcp.sport : %d\n", ntohs(tptr->th_sport));
 				printf("tcp.dport : %d\n", ntohs(tptr->th_dport));
 				printf("data\n");
@@ -48,7 +53,8 @@ int main(int argc, char *argv[])
 			}
 		}
 		printf("\n");
-	}		/* And close the session */
+	}
+
 	pcap_close(handle);
-	return(0);
+	return 0;
 }
